97d1acd9fe2646d861aed97ddcb2afbc778e682e
RynerNO <ryner.no@gmail.com>
2020-07-31 16:54:39 +0300

    fix weighting and added change fittings icon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-
+const IMAGES = {
+  PVC: {
+    bracket: 'https://cdn-ru.bitrix24.ru/b13208222/landing/5ca/5ca57ae3e21a5d437891d5f00954d117/skobapvh_2x_1x.jpg',
+      turning: 'https://cdn-ru.bitrix24.ru/b13208222/landing/caf/caf27d8c4a391f259811a267515b2b77/skobapvh1_2x_1x.jpg',
+      french: 'https://cdn-ru.bitrix24.ru/b13208222/landing/43f/43f189ddbc25d28e739abb4247a2f855/fzpvh_2x_1x.jpg',
+      deaf: 'https://cdn-ru.bitrix24.ru/b13208222/landing/ff1/ff1c5f727cfd164e2c963dafb4896b29/luv122_2x_1x.jpg'
+  },
+  Oxford: {
+    bracket: 'https://cdn-ru.bitrix24.ru/b13208222/landing/04b/04b929d2a855750f8a898831c11953d3/skoba120_2x_1x.jpg',
+    turning: 'https://cdn-ru.bitrix24.ru/b13208222/landing/ccc/cccd99a16d2690f81c3ff10be7781db9/povorotka_2x_1x.jpg',
+    french: 'https://cdn-ru.bitrix24.ru/b13208222/landing/a43/a43a4ac504fca10dba261c5bd412500f/fz1_2x_1x.jpg',
+    deaf: 'https://cdn-ru.bitrix24.ru/b13208222/landing/b22/b22a151c56d426563e45965c2b5ec933/luv_2x_1x.jpg'
+  
+},
+}
 // Цены
 const PRICES = {
   PVC: {
@@ -52,8 +66,13 @@ function priceCalc() {
   // range Утяжилитель
   this.WEIGHTING_EL = document.getElementById('weighting')
 
+  this.FITTINGS_ICON = document.getElementById('fittings__icon')
   this.TOTAL_PRICE = document.getElementById('totalPrice')
 
+  this.TOTAL_AREA_TEXT = document.querySelector('.total__area-span')
+  this.DOOR_AREA_TEXT = document.querySelector('.door__area-span') 
+  this.ZIPPER_LENGTH_TEXT = document.querySelector('.zipper__length-span')
+  this.WEIGHTING_TEXT = document.querySelector('.weighting-span')
   this._calcTotalPrice = function () {
     // Расчет цены
     let totalAreaAdditionalPrice = 0
@@ -68,15 +87,21 @@ function priceCalc() {
     if (this.polyurethane === 'true') {
       doorPrice = PRICES.misc.door.polyurethane
     }
-    totalAreaAdditionalPrice += this.weighting * 100
+    if (this.weighting > 0) {
+      totalAreaAdditionalPrice += 100
+    }
+    
+    console.log(this.weighting)
     const totalPrice = this.totalArea * (PRICES[this.complete][this.fitting] + totalAreaAdditionalPrice) + this.doorArea * doorPrice + this.zipperLength * zipperPrice
   
     this.TOTAL_PRICE.innerText = totalPrice
   }
   // Берем значения с инпутов и устанавливаем в переменные
   this._setValues = function () {
+    // Общая площадь
     this.totalArea = this.TOTAL_AREA_EL.value
 
+    // Комплект
     this.complete = this.COMPLETE_SELECT_EL.options[this.COMPLETE_SELECT_EL.selectedIndex].getAttribute('complete')
     this.POLYURETHANE_RADIO_ELS.forEach((el) => {
       if (el.checked) {
@@ -121,9 +146,12 @@ function priceCalc() {
 
 
     selectInputs.forEach((el) => {
-      el.addEventListener('change', () => {
+      el.addEventListener('change', (e) => {
         this._setValues()
         this._calcTotalPrice()
+        if (e.target === this.FITTINGS_SELECT_EL || e.target === this.COMPLETE_SELECT_EL) {
+          this.FITTINGS_ICON.src = IMAGES[this.complete][this.fitting]
+        }
       })
     })
 
@@ -140,6 +168,11 @@ function priceCalc() {
       el.addEventListener('input', () => {
         this._setValues()
         this._calcTotalPrice()
+        // 
+        this.TOTAL_AREA_TEXT.innerText = this.totalArea; 
+        this.DOOR_AREA_TEXT.innerText = this.doorArea; 
+        this.ZIPPER_LENGTH_TEXT.innerText = this.zipperLength; 
+        this.WEIGHTING_TEXT.innerText = this.weighting; 
       })
     })
   }
